287bace152d95f7e52a3a9ddbae587eaf4aad856
aliyasindogan <a.yasindogan@gmail.com>
2021-05-07 10:28:12 +0300

    Add project files.

---
+++ DevexpressRepositoryItemLookUpEditInGridView/XtraForm1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace DevexpressRepositoryItemLookUpEditInGridView
+{
+    partial class XtraForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnReaderActive = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Location = new System.Drawing.Point(3, 12);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(813, 455);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(613, 515);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(176, 23);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Write To Output (Save)";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnReaderActive
+            // 
+            this.btnReaderActive.Location = new System.Drawing.Point(405, 515);
+            this.btnReaderActive.Name = "btnReaderActive";
+            this.btnReaderActive.Size = new System.Drawing.Size(190, 23);
+            this.btnReaderActive.TabIndex = 3;
+            this.btnReaderActive.Text = "All Reader Actived";
+            this.btnReaderActive.UseVisualStyleBackColor = true;
+            this.btnReaderActive.Click += new System.EventHandler(this.btnReaderActive_Click);
+            // 
+            // XtraForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 566);
+            this.Controls.Add(this.btnReaderActive);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "XtraForm1";
+            this.Text = "GridView Example";
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnReaderActive;
+    }
+}
---
+++ DevexpressRepositoryItemLookUpEditInGridView/XtraForm1.cs
@@ -0,0 +1,137 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors.Repository;
+
+namespace DevexpressRepositoryItemLookUpEditInGridView
+{
+    public partial class XtraForm1 : DevExpress.XtraEditors.XtraForm
+    {
+        private Reader newReader = new Reader();
+        private List<Reader> newReaders = new List<Reader>();
+
+        public XtraForm1()
+        {
+            InitializeComponent();
+            InitData();
+
+            gridControl1.DataSource = Readers;
+
+            // Create an in-place LookupEdit control.
+            RepositoryItemLookUpEdit riLookup = new RepositoryItemLookUpEdit();
+            riLookup.DataSource = TimeZones;
+            riLookup.ValueMember = "Id";
+            riLookup.DisplayMember = "TimeZoneName";
+
+            riLookup.BestFitMode = DevExpress.XtraEditors.Controls.BestFitMode.BestFitResizePopup;
+            riLookup.DropDownRows = TimeZones.Count;
+
+            riLookup.SearchMode = DevExpress.XtraEditors.Controls.SearchMode.AutoComplete;
+            riLookup.AutoSearchColumnIndex = 1;
+
+            gridControl1.RepositoryItems.Add(riLookup);
+
+            gridView1.Columns["TimeZoneID"].ColumnEdit = riLookup;
+            gridView1.Columns.ColumnByFieldName("Id").OptionsColumn.AllowEdit = false;
+            gridView1.Columns.ColumnByFieldName("ReaderName").OptionsColumn.AllowEdit = false;
+            //gridView1.OptionsSelection.MultiSelect = true;
+            //gridView1.OptionsSelection.MultiSelectMode = GridMultiSelectMode.CheckBoxRowSelect;
+            //gridView1.OptionsSelection.ShowCheckBoxSelectorInColumnHeader = DefaultBoolean.True;
+
+            gridView1.CellValueChanging += GridView1_CellValueChanging;
+            gridView1.BestFitColumns();
+        }
+
+        private void GridView1_CellValueChanging(object sender, DevExpress.XtraGrid.Views.Base.CellValueChangedEventArgs e)
+        {
+            Debug.WriteLine(" Id Value: " + gridView1.GetRowCellValue(e.RowHandle, "Id") +
+                            " Column: " + e.Column.Name +
+                            " Value: " + e.Value +
+                            " RowHandle: " + e.RowHandle.ToString());
+            newReaders.Add(new Reader()
+            {
+                ReaderActive = e.Column.Name == "colReaderActive" ? (bool)e.Value : Convert.ToBoolean(gridView1.GetRowCellValue(e.RowHandle, "ReaderActive")),
+                TimeZoneID = e.Column.Name == "colTimeZoneID" ? (int)e.Value : Convert.ToInt32(gridView1.GetRowCellValue(e.RowHandle, "TimeZoneID")),
+                Id = Convert.ToInt32(gridView1.GetRowCellValue(e.RowHandle, "Id")),
+                ReaderName = Convert.ToString(gridView1.GetRowCellValue(e.RowHandle, "ReaderName")),
+            });
+        }
+
+        private List<TimeZone> TimeZones = new List<TimeZone>();
+        private List<Reader> Readers = new List<Reader>();
+
+        private void InitData()
+        {
+            var timeZones = new TimeZone[]
+            {
+                new TimeZone{Id = 1, TimeZoneName = "TimeZoneName1"},
+                new TimeZone{Id = 2, TimeZoneName = "TimeZoneName2"},
+                new TimeZone{Id = 3, TimeZoneName = "TimeZoneName3"},
+                new TimeZone{Id = 4, TimeZoneName = "TimeZoneName4"},
+            };
+            TimeZones.AddRange(timeZones);
+
+            var readers = new Reader[]
+            {
+                new Reader{Id = 1, TimeZoneID = 1,ReaderActive = false,ReaderName = "Reader1"},
+                new Reader{Id = 2, TimeZoneID = 1,ReaderActive = false,ReaderName = "Reader2"},
+                new Reader{Id = 3, TimeZoneID = 1,ReaderActive = false,ReaderName = "Reader3"},
+                new Reader{Id = 4, TimeZoneID = 1,ReaderActive = false,ReaderName = "Reader4"},
+                new Reader{Id = 5, TimeZoneID = 1,ReaderActive = false,ReaderName = "Reader5"},
+                new Reader{Id = 6, TimeZoneID = 1,ReaderActive = false,ReaderName = "Reader6"},
+                new Reader{Id = 7, TimeZoneID = 1,ReaderActive = false,ReaderName = "Reader7"},
+                new Reader{Id = 8, TimeZoneID = 1,ReaderActive = false,ReaderName = "Reader8"},
+            };
+            Readers.AddRange(readers);
+        }
+
+        private class Reader
+        {
+            public int Id { get; set; }
+            public string ReaderName { get; set; }
+            public bool ReaderActive { get; set; }
+            public int TimeZoneID { get; set; }
+        }
+
+        private class TimeZone
+        {
+            public int Id { get; set; }
+
+            public string TimeZoneName { get; set; }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            foreach (var reader in newReaders)
+            {
+                Debug.WriteLine(reader.Id + " - " + reader.ReaderName + " - " + reader.ReaderActive + " - " + reader.TimeZoneID);
+            }
+            newReaders.RemoveAll(x => x.Id > 0);
+        }
+
+        private void btnReaderActive_Click(object sender, EventArgs e)
+        {
+            List<Reader> readerActiveList = new List<Reader>();
+            foreach (var reader in Readers)
+            {
+                var data = Readers.Find(x => x.Id == reader.Id);
+                readerActiveList.Add(new Reader()
+                {
+                    Id = data.Id,
+                    ReaderActive = true,
+                    ReaderName = data.ReaderName,
+                    TimeZoneID = data.TimeZoneID,
+                });
+            }
+            gridControl1.DataSource = readerActiveList;
+        }
+    }
+}
